6a48fef88f7725342010e279b4277c373af4d339
Wasealex <wasealex27@gmail.com>
2024-07-05 14:26:46 +0300

    defining a variables with annoutations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains the following variables:
+- a: an integer with a value of 1
+- pi: a float with a value of 3.14
+- i_understand_annotations: a boolean with a value of True
+- school: a string with a value of "Holberton"
+"""
+# Variables a, pi, i_understand_annotations, and school are defined with the
+# types int, float, bool, and str, respectively.
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
