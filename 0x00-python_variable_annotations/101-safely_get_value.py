ce97a1cf617b7e9a3705c91fb0d706356eead345
Wasealex <wasealex27@gmail.com>
2024-07-05 15:10:34 +0300

    trying mapping

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,12 +6,13 @@ otherwise None.
 """
 
 
-from typing import Dict, Any, TypeVar
+from typing import Dict, Any, TypeVar, Mapping, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Dict[Any, T], key: Any, default: T = None) -> T:
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None] = None) -> Union[Any, T]:
     """
     This function takes a dictionary dct and a key key as arguments and
         returns the value of the key if it exists, otherwise None.
