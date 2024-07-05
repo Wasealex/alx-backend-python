da56577fcd90ca73f34c5fb019a6c57786a1b90a
Wasealex <wasealex27@gmail.com>
2024-07-05 15:05:19 +0300

    annoatation fix using sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,10 +6,10 @@ otherwise None.
 """
 
 
-from typing import Any, List, Union
+from typing import Any, List, Union, Sequence
 
 
-def safe_first_element(lst: List[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     This function takes a list lst of any type as argument and
       returns the first element of the list if it exists, otherwise None.
