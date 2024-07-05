4e3c8375e4ea9cfee6b5b7db5b7e0e49f3b1b580
Wasealex <wasealex27@gmail.com>
2024-07-05 14:16:58 +0300

    concantinate strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module contains the function concat that takes two arguments str1 and str2
+and returns their concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function takes two arguments str1 and str2 and returns their
+    concatenated string.
+    """
+    return str1 + str2
