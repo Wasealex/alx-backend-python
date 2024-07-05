05caa4ab27b8136b6e23ebde2e6800e902e9c844
Wasealex <wasealex27@gmail.com>
2024-07-05 14:34:29 +0300

    retrun type fixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -13,4 +13,4 @@ def sum_mixed_list(mxd_lst: List[int, float]) -> float:
     This function takes a list mxd_lst of integers and floats as argument and
     returns their sum as a float.
     """
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))
