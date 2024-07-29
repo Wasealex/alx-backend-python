c9b204dd00b3a8d96813ffb78326e4d78aedabad
Wasealex <wasealex27@gmail.com>
2024-07-29 10:39:23 +0300

    test memoization for returning 42

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,10 @@
 """unittests using nested maps.
 """
 import unittest
-from unittest import mock
+from unittest import TestCase, mock
 from parameterized import parameterized
 from requests import patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import Dict, Mapping, Sequence
 
 
@@ -62,7 +62,22 @@ class TestGetJson(unittest.TestCase):
         mock_request_get.assert_called_with(test_url)
 
 class TestMemoize(unittest.TestCase):
-    pass
+    """test memoize class"""
+    def test_memoize(self)  -> None:
+        """Test memoize method"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+        with patch.object(TestClass, "a_method") as mock_method:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
